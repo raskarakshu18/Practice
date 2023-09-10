25e23202e12ffd70efa53a91aa6278c27a651a81
Your Name <you@example.com>
2023-09-10 11:43:45 +0530

    Changes done

--- CheckClass.java
+++ CheckClass.java
@@ -1,9 +1,9 @@
 package com.java.vote;
-
+//changes made
 public class CheckClass {
 
 	public static boolean checkeligibity(int age) {
-		if (age == 18) {
+		if (age == 20) {
 			return true;
 
 		} else
